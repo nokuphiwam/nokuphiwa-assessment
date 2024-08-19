19d3a386654d52834ed2afc0269e9ecd3a0cee28
nokuphiwam <49691242+nokuphiwam@users.noreply.github.com>
2024-08-19 14:07:57 +0200

    Create SQL.cs

---
+++ SQL.cs
@@ -0,0 +1,32 @@
+string sql = "SELECT TOP 1000000 * FROM received WHERE status = 1 ORDER BY re_ref";
+
+// List of SQL nodes to query
+IEnumerable<IConfigurationSection> sqlNodes = Program.Configuration
+    .GetSection("ConnectionStrings")
+    .GetSection("SqlNodes")
+    .GetChildren();
+
+// Merged results set
+List<received> results = new List<received>();
+
+// Use Parallel.ForEach with MaxDegreeOfParallelism to control concurrent queries
+ParallelOptions parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 5 };
+Parallel.ForEach(sqlNodes, parallelOptions, node =>
+{
+    received[] result = DBQuery<received>.Query(node.Value, sql);
+    lock (results) // Synchronize access to shared list
+    {
+        results.AddRange(result);
+    }
+});
+
+// Use bulk insert instead of individual inserts
+using (SqlConnection connection = new SqlConnection(ConnectionString))
+{
+    connection.Open();
+    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+    {
+        bulkCopy.DestinationTableName = "received_total";
+        bulkCopy.WriteToServer(results);
+    }
+}
